c81cdfa55ce8db483ec132b4720dfd25e681e53b
Aisha Alkrz <105807117+aishaalkrz@users.noreply.github.com>
2026-06-05 17:13:51 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     * 
+     * 
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false 
+    
+        let count = new Array(26).fill(0)
+
+        for ( let i =0; i < s.length; i++){
+            count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++
+            count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--
+        }
+
+        for( let i = 0; i < 26 ; i++){
+            if(count[i] !== 0 ) return false
+        }
+        return true
+    }
+}
