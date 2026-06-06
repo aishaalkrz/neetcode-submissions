d6da662dfcdaff0a460cfd6b3efcd4fb64745e0e
Aisha Alkrz <105807117+aishaalkrz@users.noreply.github.com>
2026-06-06 08:51:27 +0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let sum = 0
+        let result = [];
+
+        for(let i=0; i<nums.length; i++){
+             for(let j=i+1; j<nums.length; j++){
+                if((nums[i] + nums[j]) == target){
+                     return result = [i,j]
+                }
+            }
+        }
+    }
+}
